c284360a5f95350efd95e0a52fc59438e9beeb42
wsq94317 <wsq94317@vip.qq.com>
2023-08-30 22:48:47 +1000

    Initial commit

---
+++ ue5_login/src/main/java/com/percy/ue5_login/Ue5LoginApplication.java
@@ -0,0 +1,13 @@
+package com.percy.ue5_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ue5LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ue5LoginApplication.class, args);
+	}
+
+}
---
+++ ue5_login/src/test/java/com/percy/ue5_login/Ue5LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percy.ue5_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ue5LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
